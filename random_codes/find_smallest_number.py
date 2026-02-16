409a459d7e4fcd9ed3cbebd8a5fa6addc64485dc
Rohan Bhande <rohanbhande11@gmail.com>
2026-02-16 17:39:28 -0600

    modified error in find_smallest_number

--- random_codes/find_smallest_number.py
+++ random_codes/find_smallest_number.py
@@ -5,7 +5,7 @@ class Solution:
     def find_smallest_number(*, array_of_elements : array) -> int:
         smallest = array_of_elements[0]
         smallest_index = 0
-        for i in range (1, len(array_of_elements) - 1):
+        for i in range (1, len(array_of_elements)):
             if (array_of_elements[i] < smallest):
                 print(f"Smallest number is : {array_of_elements[i]}")
                 smallest = array_of_elements[i]
@@ -13,5 +13,5 @@ class Solution:
         return smallest
             
 if __name__ == "__main__":
-    arr = [42, 89, 93, 15, 68, 24, 81, 3, 56, 19, 77, 10, 99, 35, 62]
+    arr = [42, 89, 93, 15, 68, 24, 81, 3, 56, 19, 77, 1, 99, 35, 62]
     print(Solution.find_smallest_number(array_of_elements=arr))
